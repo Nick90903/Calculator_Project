7bea021ad43a722a29eea5953e73a3f9182d8928
nick90903 <nhatchew03@gmail.com>
2022-04-03 05:33:13 -0500

    Added input text updates

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let buttons = Array.from(document.querySelectorAll('.num_op'));
+let outputField = document.querySelector('.input_screen');
+let outputText = '';
 let action = 0;
 let firstNumber = [];
 let secondNumber = [];
 let operator;
 
 console.table(buttons);
+console.log(outputField);
 buttons.forEach(item => item.addEventListener('click', function() {
     buttonClicked(item.textContent);
 }));
@@ -13,15 +16,21 @@ function buttonClicked(btnID) {
     console.log(btnID);
     if(action == 0 && btnID >= 0 && btnID <= 9) {
         firstNumber.push(btnID);
-        console.log(firstNumber);
+        outputText = firstNumber.join('');
+        outputField.textContent = outputText;
+        console.log(`Array ${firstNumber} Text ${outputText}`);
     } else if (action == 1 && btnID >= 0 && btnID <= 9) {
         secondNumber.push(btnID);
+        outputText = outputText + ` ${secondNumber}`;
+        outputField.textContent = outputText;
         console.log(secondNumber);
     } else if (btnID == 'C') {
         //CLEAR CALCULATOR
     } else if (btnID == '*' || btnID == '/' || btnID == '+' || btnID == '-') {
         if(action != 1){
             operator = btnID;
+            outputText = outputText + ` ${operator}`;
+            outputField.textContent = outputText;
             console.log(operator);
             action = 1;
         }else if(btnID == 'Calculate' && secondNumber.length > 0) {
